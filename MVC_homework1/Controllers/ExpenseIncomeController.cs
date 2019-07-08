1ddcecf504d0956af321affdfd6523650a1e1228
allen <allen@gis.tw>
2019-07-08 11:32:04 +0800

    新增記帳本畫面

---
+++ MVC_homework1/Controllers/ExpenseIncomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_homework1.ViewModels;
+
+namespace MVC_homework1.Controllers
+{
+    public class ExpenseIncomeController : Controller
+    {
+        // GET: ExpenseIncome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+        [ChildActionOnly]
+        public ActionResult List(int page=1,int pageCount=100)
+        {
+            //int page = 1;
+            //int pageCount = 100;
+
+            ViewData["pageItemIndex"] =( (page-1) *  pageCount)+1;
+
+            List<ExpenseIncomeViewModel> expenseIncomeList = new List<ExpenseIncomeViewModel>();
+            for (int i = 0; i < 100; i++)
+            {
+                Random gen = new Random(Guid.NewGuid().GetHashCode());
+                int prob = gen.Next(20000);
+                int type = gen.Next(0,2);
+                
+                DateTime Day = new DateTime(2019,1,1).AddDays(gen.Next(0, 365));
+ 
+
+
+                expenseIncomeList.Add(new ExpenseIncomeViewModel
+                {
+                    Money = prob,
+                    ExpenseIncometype = (type==1) ?"收入":"支出",
+                    CreateTime = Day
+
+                });
+            }
+            return View(expenseIncomeList.OrderBy(c=>c.CreateTime).Take(pageCount));
+        }
+
+
+    }
+}
---
+++ MVC_homework1/ViewModels/ExpenseIncomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_homework1.ViewModels
+{
+    public class ExpenseIncomeViewModel
+    {
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        [DisplayName("金額")]
+        public decimal Money { get; set; }
+        [DisplayName("類別")]
+        public string ExpenseIncometype { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("日期")]
+        public DateTime CreateTime { get; set; }
+        [DisplayName("備註")]
+        public string Remark { get; set; }
+
+
+    }
+}
