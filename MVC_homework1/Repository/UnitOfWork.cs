f1ab4e1b32efb81c016d02ef061fe61bd53b80c2
allen <allen@gis.tw>
2019-07-15 11:50:09 +0800

    新增資料庫 & Repository & unitofwork

---
+++ MVC_homework1/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using MVC_homework1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_homework1.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new SkillTreeHomeworkContext();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
