43c704acd2f1e6223ad4221d9d7cb2ce047752ee
allen <allen@gis.tw>
2019-07-15 13:14:51 +0800

    記帳本 _Create -> Create (partial view -> child action ) 實作串接Service

--- MVC_homework1/Controllers/ExpenseIncomeController.cs
+++ MVC_homework1/Controllers/ExpenseIncomeController.cs
@@ -4,47 +4,70 @@ using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_homework1.Repository;
+using MVC_homework1.Service;
 using MVC_homework1.ViewModels;
 
 namespace MVC_homework1.Controllers
 {
     public class ExpenseIncomeController : Controller
     {
+        private readonly AccountBookService _accountBookService;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly ClassifyService _classifyService;
+
+        public ExpenseIncomeController()
+        {
+            _unitOfWork = new UnitOfWork();
+            _accountBookService = new AccountBookService(_unitOfWork);
+            _classifyService = new ClassifyService();
+        }
+
         // GET: ExpenseIncome
         public ActionResult Index()
         {
             return View();
         }
-        
 
         [ChildActionOnly]
-        public ActionResult List(int page=1,int pageCount=100)
+        public ActionResult Create()
+        {
+            ViewData["TypeList"] = _classifyService.getExpenseIncomeTypeToSelectList();
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Money,ExpenseIncometype,Remark")]
+                                   ExpenseIncomeViewModel expenseIncomeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _accountBookService.Add(expenseIncomeViewModel);
+                _unitOfWork.Commit();
+                ModelState.Clear();
+            }
+            ViewData["TypeList"] = _classifyService.getExpenseIncomeTypeToSelectList();
+
+            return View();
+        }
+
+
+        [ChildActionOnly]
+        public ActionResult List(int page = 1, int pageCount = 100)
         {
             //int page = 1;
             //int pageCount = 100;
+            var skipCount = ((page - 1) * pageCount);
+            ViewData["pageItemIndex"] = skipCount + 1;
 
-            ViewData["pageItemIndex"] =( (page-1) *  pageCount)+1;
+            var expenseIncomeList = _accountBookService.Lookup()
+                                                        .OrderByDescending(c => c.CreateTime)
+                                                        .Skip(skipCount)
+                                                        .Take(pageCount);
 
-            List<ExpenseIncomeViewModel> expenseIncomeList = new List<ExpenseIncomeViewModel>();
-            for (int i = 0; i < 100; i++)
-            {
-                Random gen = new Random(Guid.NewGuid().GetHashCode());
-                int prob = gen.Next(20000);
-                int type = gen.Next(0,2);
-                
-                DateTime Day = new DateTime(2019,1,1).AddDays(gen.Next(0, 365));
- 
-
-
-                expenseIncomeList.Add(new ExpenseIncomeViewModel
-                {
-                    Money = prob,
-                    ExpenseIncometype = (type==1) ?"收入":"支出",
-                    CreateTime = Day
-
-                });
-            }
-            return View(expenseIncomeList.OrderBy(c=>c.CreateTime).Take(pageCount));
+            return View(expenseIncomeList.ToList());
         }
 
 
