2622b8064d0f669aa7f12e9d6084b34422945327
allen <allen@gis.tw>
2019-07-24 18:38:48 +0800

    新增Filter

---
+++ MVC_homework1/Filters/AjaxOnlyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_homework1.Filters
+{
+    public class AjaxOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsAjaxRequest())
+                filterContext.HttpContext.Response.Redirect("Error");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+
+    }
+}
---
+++ MVC_homework1/Filters/BeforeCurrentDateAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_homework1.Filters
+{
+    public class BeforeCurrentDateAttribute : ValidationAttribute
+    {
+        public BeforeCurrentDateAttribute()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                var dt = (DateTime)value;
+                if (dt >= DateTime.Now)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
